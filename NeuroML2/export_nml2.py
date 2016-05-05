1256adb06ce5c92e41743b84fd1c8d5253321136
Andris Ecker <ecker.andris@gmail.com>
2016-05-05 17:58:53 +0100

    morphology exported

--- NeuroML2/export_nml2.py
+++ NeuroML2/export_nml2.py
@@ -4,5 +4,7 @@ from pyneuroml.neuron import export_to_neuroml2
 export_to_neuroml2("../NEURON/tester.hoc", 
                    "test.cell.nml", 
                    includeBiophysicalProperties=True,
-                   known_rev_potentials={'Ih':-32})
+                   known_rev_potentials={'Ih':-32.9, 'cal':0, 'cat':0,
+                                         'kca':-95, 'Ika':-95, 'IM':-95, 'Ikdrf':-95, 'Ikdrs':-95, 'passsd':-70,
+                                         'Ikdrfaxon':-95, 'Ikdrsaxon':-95, 'passaxon':-70})
 
